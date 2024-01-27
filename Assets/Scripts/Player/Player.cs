c98b842eaf6af76a924ebfd1295b1420737ef861
Bowie <cirosilv@gmail.com>
2024-01-26 21:18:28 -0300

    Adding Animation in Jump movement with DOTween

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
@@ -11,10 +12,19 @@ public class Player : MonoBehaviour
     public float speed = 10f;
     public float speedRun = 5f;
 
+    //private bool _isRunning = false;
+
     private float _currentSpeed;
 
     [Header("CONTROLE JUMP")]
     public float forceJump = 10f;
+
+    [Header("ANIMATION SETUP")]
+    public float scaleJumpY = 1.5f;
+    public float scaleJumpX = .5f;
+    public float duratioAnim = .3f;
+    public float delayAnim = .2f;
+    public Ease ease = Ease.OutBack;
 
     void Start()
     {
@@ -28,6 +38,10 @@ public class Player : MonoBehaviour
     }
     private void HandleMovement()
     {
+
+        //_isRunning = Input.GetKey(KeyCode.LeftShift);
+
+
         if (Input.GetKey(KeyCode.LeftShift))
             _currentSpeed = speedRun;
         else
@@ -36,6 +50,11 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             rb.velocity = new Vector2(_currentSpeed, rb.velocity.y);
+            /*if (_isRunning)
+                rb.velocity = new Vector2(speedRun, rb.velocity.y);
+            else
+                rb.velocity = new Vector2(speed, rb.velocity.y);
+            */
             //rb.MovePosition(rb.position + velocity * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.A)){
@@ -58,6 +77,16 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.velocity = Vector2.up * forceJump;
+            rb.transform.localScale = Vector2.one;
+
+            DOTween.Kill(rb.transform);
+
+            HandleScaleJump();
         }
+    }
+    private void HandleScaleJump()
+    {
+        rb.transform.DOScaleY(scaleJumpY, duratioAnim).SetEase(ease).SetDelay(delayAnim).SetLoops(2, LoopType.Yoyo);
+        rb.transform.DOScaleX(scaleJumpX, duratioAnim).SetEase(ease).SetDelay(delayAnim).SetLoops(2, LoopType.Yoyo);
     }
 }
