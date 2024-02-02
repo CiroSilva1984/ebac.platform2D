ad2f375fdf515aaf75a13bcf1c58acf6e0677e3b
Bowie <cirosilv@gmail.com>
2024-02-01 23:37:59 -0300

    Manipulating the animator with code

--- Assets/Scripts/Animations/AnimatorTest.cs
+++ Assets/Scripts/Animations/AnimatorTest.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class AnimatorTest : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+
+    public string triggerToPlay = "Fly";
+
+    private void OnValidate()
     {
-        
+        if (animator == null) animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            animator.SetTrigger(triggerToPlay);
+        }
     }
 }
